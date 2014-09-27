b0cd3d371b9dccdc3e19113f633c1288e52f765a
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-09-28 01:44:43 +0200

    Detects `QuerySet.extra` select & where arguments and avoid caching such queries.
    Also fixes tables detection so that creation/deletion correctly invalidates queries.

--- cachalot/monkey_patch.py
+++ cachalot/monkey_patch.py
@@ -2,11 +2,14 @@
 
 from __future__ import unicode_literals
 from collections import Iterable
+import re
+
 from django.core.cache import cache
 from django.db.models.query import EmptyResultSet
 from django.db.models.sql.compiler import (
     SQLCompiler, SQLAggregateCompiler, SQLDateCompiler, SQLDateTimeCompiler,
     SQLInsertCompiler, SQLUpdateCompiler, SQLDeleteCompiler)
+from django.db.models.sql.where import ExtraWhere
 
 
 COMPILERS = (SQLCompiler,
@@ -20,16 +23,22 @@ PATCHED = False
 MISS_VALUE = '[[The cache key was missed]]'
 
 
-def _get_tables_cache_keys(compiler):
-    q = compiler.query
-    # FIXME: `.extra` (and maybe more) are not in alias_map
-    tables = q.alias_map.keys()
+def _get_tables(query):
+    """
+    Returns a ``set`` of all database table names used by ``query``.
+    """
+    tables = query.used_aliases.copy()
+    tables.add(query.model._meta.db_table)
+    tables.update(query.extra_tables)
+    return tables
 
-    return ['%s_queries' % t for t in tables]
+
+def _get_tables_cache_keys(query):
+    return ['%s_queries' % t for t in _get_tables(query)]
 
 
-def _update_tables_queries(compiler, cache_key):
-    tables_cache_keys = _get_tables_cache_keys(compiler)
+def _update_tables_queries(query, cache_key):
+    tables_cache_keys = _get_tables_cache_keys(query)
     tables_queries = cache.get_many(tables_cache_keys)
     for k in tables_cache_keys:
         queries = tables_queries.get(k, [])
@@ -38,8 +47,8 @@ def _update_tables_queries(compiler, cache_key):
     cache.set_many(tables_queries)
 
 
-def _invalidate_tables(compiler):
-    tables_cache_keys = _get_tables_cache_keys(compiler)
+def _invalidate_tables(query):
+    tables_cache_keys = _get_tables_cache_keys(query)
     tables_queries = cache.get_many(tables_cache_keys)
     queries = []
     for k in tables_cache_keys:
@@ -48,10 +57,45 @@ def _invalidate_tables(compiler):
     cache.delete_many(tables_cache_keys)
 
 
+COLUMN_RE = re.compile(r'^"(?P<table>[\w_]+)"\."(?P<column>[\w_]+)"$')
+
+
+def _has_extra_select_or_where(query):
+    """
+    Returns True if ``query`` contains a ``QuerySet.extra`` with ``select``
+    or ``where`` arguments.
+
+    We also have to check for ``prefetch_related``, as it internally uses a
+    ``QuerySet.extra(select={'_prefetch_related_val_…': '"table"."column"'})``.
+
+    For more details on how prefetch_related uses ``QuerySet.extra``, see:
+    https://github.com/django/django/blob/1.6.7/django/db/models/fields/related.py#L553-L577
+    """
+
+    # Checks if there’s an extra where
+    if any(isinstance(child, ExtraWhere) for child in query.where.children):
+        return True
+
+    # Checks if there’s an extra select
+    if query.extra_select and query.extra_select_mask is None:
+        tables = _get_tables(query)
+        # Checks if extra subqueries are something else than one or several
+        # ``prefetch_related``.
+        for subquery, params in query.extra_select.values():
+            match = COLUMN_RE.match(subquery)
+            return match is None or match.group('table') not in tables
+    return False
+
+
 def _monkey_patch_orm_read():
     def patch_execute_sql(method):
         def inner(compiler, *args, **kwargs):
             if isinstance(compiler, WRITE_COMPILERS):
+                return method(compiler, *args, **kwargs)
+
+            query = compiler.query
+
+            if _has_extra_select_or_where(query):
                 return method(compiler, *args, **kwargs)
 
             try:
@@ -67,7 +111,7 @@ def _monkey_patch_orm_read():
                         and not isinstance(result, (tuple, list)):
                     result = list(result)
 
-                _update_tables_queries(compiler, cache_key)
+                _update_tables_queries(query, cache_key)
 
                 cache.set(cache_key, result)
 
@@ -82,7 +126,7 @@ def _monkey_patch_orm_read():
 def _monkey_patch_orm_write():
     def patch_execute_sql(method):
         def inner(compiler, *args, **kwargs):
-            _invalidate_tables(compiler)
+            _invalidate_tables(compiler.query)
             return method(compiler, *args, **kwargs)
         return inner
 
