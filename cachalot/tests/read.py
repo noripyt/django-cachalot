ae0ca7310831c03b62ea4adfbeb02ec2846d8dd7
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-11-03 23:49:20 +0100

    Revert "Small simplification in a test."
    This reverts commit c0845682255a9b108c394a5285d8605ab48bccb6.

--- cachalot/tests/read.py
+++ cachalot/tests/read.py
@@ -535,12 +535,14 @@ class ReadTestCase(TransactionTestCase):
         sql = 'SELECT * FROM %s;' % Test._meta.db_table
 
         with self.assertNumQueries(1):
-            with connection.cursor() as cursor:
-                cursor.execute(sql)
-                data1 = list(cursor.fetchall())
+            cursor = connection.cursor()
+            cursor.execute(sql)
+            data1 = list(cursor.fetchall())
+            cursor.close()
         with self.assertNumQueries(1):
-            with connection.cursor() as cursor:
-                cursor.execute(sql)
-                data2 = list(cursor.fetchall())
+            cursor = connection.cursor()
+            cursor.execute(sql)
+            data2 = list(cursor.fetchall())
+            cursor.close()
         self.assertListEqual(data2, data1)
         self.assertListEqual(data2, list(Test.objects.values_list()))
