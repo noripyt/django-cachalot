73d09b2e5f6b5bcceee550736bc42533c8ab5f3c
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-11-24 12:04:01 +0100

    Improves benchmark plots.

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals, print_function
 import os
+from random import choice
 from time import time
 
 if __name__ == '__main__':
@@ -51,7 +52,7 @@ class Benchmark(object):
     def bench_once(self, context, num_queries, invalidate_before=False):
         for _ in range(self.n):
             if invalidate_before:
-                clear(db_alias=self.db_alias)
+                invalidate_all(db_alias=self.db_alias)
             with AssertNumQueries(num_queries, using=self.db_alias):
                 start = time()
                 self.query_function(self.db_alias)
@@ -80,12 +81,12 @@ class Benchmark(object):
     def execute_benchmark(self):
         self.benchmark('.count()', to_list=False)
         self.benchmark('.first()', to_list=False)
-        self.benchmark('[:5]')
-        self.benchmark(".filter(name__icontains='e')[:5]")
-        self.benchmark(".order_by('owner')[:5]")
-        self.benchmark(".select_related('owner')[:5]")
+        self.benchmark('[:10]')
+        self.benchmark(".filter(name__icontains='e')[:10]")
+        self.benchmark(".order_by('owner')[:10]")
+        self.benchmark(".select_related('owner')[:10]")
         self.benchmark(
-            ".select_related('owner').prefetch_related('owner__groups')[:5]",
+            ".select_related('owner').prefetch_related('owner__groups')[:10]",
             num_queries=2)
 
     def run(self):
@@ -104,16 +105,34 @@ class Benchmark(object):
             os.mkdir(RESULTS_PATH)
         self.df.to_csv(os.path.join(RESULTS_PATH, 'data.csv'))
 
+        self.xlim = (0, self.df['time'].max() * 1.01)
         self.output('db')
         self.output('cache')
 
     def output(self, param):
         gp = self.df.groupby(('context', 'query', param))['time']
-        self.means = gp.mean().unstack().unstack().reindex(CONTEXTS[::-1])
-        self.stds = gp.std().unstack().unstack().reindex(CONTEXTS[::-1])
-
+        self.means = gp.mean().unstack().unstack().reindex(CONTEXTS)
+        los = self.means - gp.min().unstack().unstack().reindex(CONTEXTS)
+        ups = gp.max().unstack().unstack().reindex(CONTEXTS) - self.means
+        self.errors = dict(
+            (key, dict(
+                (subkey,
+                 [[los[key][subkey][context] for context in self.means.index],
+                  [ups[key][subkey][context] for context in self.means.index]])
+                for subkey in self.means.columns.levels[1]))
+            for key in self.means.columns.levels[0])
         self.get_perfs()
-        self.plot()
+        self.plot_detail(param)
+
+        gp = self.df.groupby(('context', param))['time']
+        self.means = gp.mean().unstack().reindex(CONTEXTS)
+        los = self.means - gp.min().unstack().reindex(CONTEXTS)
+        ups = gp.max().unstack().reindex(CONTEXTS) - self.means
+        self.errors = [
+            [[los[key][context] for context in self.means.index],
+             [ups[key][context] for context in self.means.index]]
+            for key in self.means]
+        self.plot_general(param)
 
     def get_perfs(self):
         for v in self.means.columns.levels[0]:
@@ -122,21 +141,27 @@ class Benchmark(object):
                   % (v.ljust(10), g[CONTEXTS[1]] / g[CONTEXTS[0]],
                      g[CONTEXTS[0]] / g[CONTEXTS[2]]))
 
-    def plot(self):
-        xlim = (0, (self.means + self.stds).unstack().max() * 1.05)
+    def plot_detail(self, param):
         for v in self.means.columns.levels[0]:
             plt.figure()
-            self.means[v].plot(
-                kind='barh', xerr=self.stds[v], title=v,
-                xlim=xlim, figsize=(15, 10), subplots=True, layout=(4, 2),
+            axes = self.means[v].plot(
+                kind='barh', xerr=self.errors[v],
+                xlim=self.xlim, figsize=(15, 10), subplots=True, layout=(4, 2),
                 sharey=True, legend=False)
-            plt.savefig(os.path.join(RESULTS_PATH, '%s_detail.svg' % v))
+            for row in axes:
+                for ax in row:
+                    ax.invert_yaxis()
+                    ax.set_ylabel('')
+                    ax.set_xlabel('Time (s)')
+            plt.savefig(os.path.join(RESULTS_PATH, '%s_%s.svg' % (param, v)))
 
-            plt.figure()
-            self.means[v].mean(axis=1).plot(
-                kind='barh', xerr=self.stds[v].mean(axis=1), title=v,
-                xlim=xlim)
-            plt.savefig(os.path.join(RESULTS_PATH, '%s.svg' % v))
+    def plot_general(self, param):
+        plt.figure()
+        self.means.plot(kind='barh', xerr=self.errors, xlim=self.xlim)
+        plt.gca().invert_yaxis()
+        plt.ylabel('')
+        plt.xlabel('Time (s)')
+        plt.savefig(os.path.join(RESULTS_PATH, '%s.svg' % param))
 
 
 def create_data(using):
@@ -144,12 +169,12 @@ def create_data(using):
         [User(username='user%d' % i) for i in range(50)])
     Group.objects.using(using).bulk_create(
         [Group(name='test%d' % i) for i in range(10)])
+    groups = list(Group.objects.using(using))
     for u in User.objects.using(using):
-        u.groups.add(*list(Group.objects.using(using).order_by('?')[:2]))
+        u.groups.add(choice(groups), choice(groups))
+    users = list(User.objects.using(using))
     Test.objects.using(using).bulk_create(
-        [Test(name='test%d' % i,
-              owner=User.objects.using(using).order_by('?')[0])
-         for i in range(10000)])
+        [Test(name='test%d' % i, owner=choice(users)) for i in range(10000)])
 
 
 if __name__ == '__main__':
