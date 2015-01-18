c3223048f126acc3de3fef86ee582defc8697926
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-01-18 17:50:35 +0100

    Fixes tests for Django 1.6 & 1.7.

--- cachalot/tests/migrations/0001_initial.py
+++ cachalot/tests/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0005_alter_user_last_login_null'),
+        ('auth', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
