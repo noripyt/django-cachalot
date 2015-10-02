ce32ac4c41b8dc3e24777eaed4364495b33d235a
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-10-03 00:10:08 +0200

    Removes the useless explicit app_label since Django 1.7.

--- cachalot/tests/models.py
+++ cachalot/tests/models.py
@@ -18,20 +18,13 @@ class Test(Model):
     bin = BinaryField(null=True, blank=True)
 
     class Meta(object):
-        app_label = 'cachalot'
         ordering = ('name',)
 
 
 class TestParent(Model):
     name = CharField(max_length=20)
 
-    class Meta(object):
-        app_label = 'cachalot'
-
 
 class TestChild(TestParent):
     public = BooleanField(default=False)
     permissions = ManyToManyField('auth.Permission', blank=True)
-
-    class Meta(object):
-        app_label = 'cachalot'
