509128a85678b729d55187207d9ed86e9fe1ef6b
Jayden Kneller <jayden@emergence.com>
2017-07-18 15:53:52 -0600

    Changes to the disabling tests after being able to run them and see what needed fixing.

--- cachalot/tests/disabling.py
+++ cachalot/tests/disabling.py
@@ -67,172 +67,280 @@ class DisablingTestCase(TestUtilsMixin, TransactionTestCase):
 
     def test_read_disabling_using_with_stmt(self):
         with self.assertNumQueries(1):
-            n1 = bool(Test.objects.exists())  # Force the query to run
-        # Caching disabled so this query shouldn't be cached
-        with DISABLE_CACHING:
-            with self.assertNumQueries(1):
-                n2 = bool(Test.objects.exists())  # Force the query to run
-
-    def test_read_toggle_disabling_using_with_stmt(self):
-        with self.assertNumQueries(1):
-            n1 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
         # Disable the cache
         with DISABLE_CACHING:
             with self.assertNumQueries(1):
-                n2 = bool(Test.objects.exists())  # Force the query to run
+                bool(Test.objects.exists())  # Force the query to run
         # Caching enabled, invalidating has run
         with self.assertNumQueries(1):
-            n3 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
         with self.assertNumQueries(0):
-            n4 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
 
-    def test_read_toggle_disabling_without_invalidating_using_with_stmt(self):
+    def test_read_disabling_without_invalidating_using_with_stmt(self):
         with self.assertNumQueries(1):
-            n1 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
         # Disable the cache
         with DISABLE_CACHING:
             DISABLE_CACHING.do_not_invalidate()
             with self.assertNumQueries(1):
-                n2 = bool(Test.objects.exists())  # Force the query to run
+                bool(Test.objects.exists())  # Force the query to run
         # Caching enabled without invalidating so this query should be cached
         with self.assertNumQueries(0):
-            n3 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
 
     def test_read_disabling_using_try_finally(self):
         with self.assertNumQueries(1):
-            n1 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
         blew_up = False
-        err = None
-        # Caching disabled so this query shouldn't be cached
-        try:
-            DISABLE_CACHING.enable()
-            with self.assertNumQueries(1):
-                n2 = bool(Test.objects.exists())  # Force the query to run
-        except Exception as err:
-            blew_up = True
-        finally:
-            DISABLE_CACHING.disable()
-        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(err))
-
-    def test_read_toggle_disabling_using_try_finally(self):
-        with self.assertNumQueries(1):
-            n1 = bool(Test.objects.exists())  # Force the query to run
-        blew_up = False
-        err = None
+        error_message = None
         # Disable the cache
         try:
             DISABLE_CACHING.enable()
             with self.assertNumQueries(1):
-                n2 = bool(Test.objects.exists())  # Force the query to run
-        except Exception as err:
+                bool(Test.objects.exists())  # Force the query to run
+        except Exception as err:  # In python 3 err is deleted after this block
             blew_up = True
+            error_message = err
         finally:
             DISABLE_CACHING.disable()
-        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(err))
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(error_message))
         # Caching enabled but invalidating has run
         with self.assertNumQueries(1):
-            n3 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
         with self.assertNumQueries(0):
-            n4 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
 
-    def test_read_toggle_disabling_without_invalidating_using_try_finally(self):
+    def test_read_disabling_without_invalidating_using_try_finally(self):
         with self.assertNumQueries(1):
-            n1 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
         blew_up = False
-        err = None
+        error_message = None
         # Disable the cache
         try:
             DISABLE_CACHING.enable()
             with self.assertNumQueries(1):
-                n2 = bool(Test.objects.exists())  # Force the query to run
-        except Exception as err:
+                bool(Test.objects.exists())  # Force the query to run
+        except Exception as err:  # In python 3 err is deleted after this block
             blew_up = True
+            error_message = err
         finally:
             DISABLE_CACHING.disable(invalidate_cache=False)
-        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(err))
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(error_message))
         # Caching enabled without invalidating so this query should be cached
         with self.assertNumQueries(0):
-            n3 = bool(Test.objects.exists())  # Force the query to run
+            bool(Test.objects.exists())  # Force the query to run
 
     def test_write_disabling_using_with_stmt(self):
-        data1 = Test.objects.get(name='test1')
-        data1_name = data1.name  # Force the query to run
+        with self.assertNumQueries(1):
+            data1 = Test.objects.get(name='test1')
         # Disable the cache
         with DISABLE_CACHING:
-            with self.assertNumQueries(1):
+            with self.assertNumQueries(2 if self.is_sqlite else 1):
                 data1.name = 'test1a'
                 data1.save()
         # Caching enabled, invalidating has run
         with self.assertNumQueries(1):
-            data2 = Test.objects.get(name='test1a')
-            data2_name = data2.name  # Force the query to run
+            Test.objects.get(name='test1a')
         with self.assertNumQueries(0):
-            data3 = Test.objects.get(name='test1a')
-            data3_name = data3.name  # Force the query to run
-        # Reset the name to test1
-        data1.name = 'test1'
-        data1.save()
+            Test.objects.get(name='test1a')
 
     # HOPEFULLY NO ONE EVER DOES THIS IN PRODUCTION CODE.
     def test_write_disabling_without_invalidating_using_with_stmt(self):
-        data1 = Test.objects.get(name='test1')
-        data1_name = data1.name  # Force the query to run
+        with self.assertNumQueries(1):
+            data1 = Test.objects.get(name='test1')
         # Disable the cache
         with DISABLE_CACHING:
             DISABLE_CACHING.do_not_invalidate()
-            with self.assertNumQueries(1):
+            with self.assertNumQueries(2 if self.is_sqlite else 1):
                 data1.name = 'test1a'
                 data1.save()
         # Caching enabled without invalidating so this query should
         # be cached even though the object is no longer named test1.
         with self.assertNumQueries(0):
-            data2 = Test.objects.get(name='test1')
-            data2_name = data2.name  # Force the query to run
+            Test.objects.get(name='test1')
         with self.assertNumQueries(1):
-            data3 = Test.objects.get(name='test1a')
-            data3_name = data3.name  # Force the query to run
-        data1.name = 'test1'
-        data1.save()
+            Test.objects.get(name='test1a')
 
     def test_write_disabling_using_try_finally(self):
-        data1 = Test.objects.get(name='test1')
-        data1_name = data1.name  # Force the query to run
+        with self.assertNumQueries(1):
+            data1 = Test.objects.get(name='test1')
         blew_up = False
-        err = None
+        error_message = None
         # Disable the cache
         try:
             DISABLE_CACHING.enable()
-            with self.assertNumQueries(1):
+            with self.assertNumQueries(2 if self.is_sqlite else 1):
                 data1.name = 'test1a'
                 data1.save()
-        except Exception as err:
+        except Exception as err:  # In python 3 err is deleted after this block
             blew_up = True
+            error_message = err
         finally:
             DISABLE_CACHING.disable()
-        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(err))
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(error_message))
         # Caching enabled, invalidating has run
         with self.assertNumQueries(1):
-            data2 = Test.objects.get(name='test1a')
-            data2_name = data2.name  # Force the query to run
+            Test.objects.get(name='test1a')
         with self.assertNumQueries(0):
-            data3 = Test.objects.get(name='test1a')
-            data3_name = data3.name  # Force the query to run
-        # Reset the name to test1
-        data1.name = 'test1'
-        data1.save()
+            Test.objects.get(name='test1a')
 
     # HOPEFULLY NO ONE EVER DOES THIS IN PRODUCTION CODE.
     def test_write_disabling_without_invalidating_using_try_finally(self):
         data1 = Test.objects.get(name='test1')
-        data1_name = data1.name  # Force the query to run
+        blew_up = False
+        error_message = None
+        # Disable the cache
+        try:
+            DISABLE_CACHING.enable()
+            with self.assertNumQueries(2 if self.is_sqlite else 1):
+                data1.name = 'test1a'
+                data1.save()
+        except Exception as err:  # In python 3 err is deleted after this block
+            blew_up = True
+            error_message = err
+        finally:
+            DISABLE_CACHING.disable(invalidate_cache=False)
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(error_message))
+        # Caching enabled without invalidating so this query should
+        # be cached even though the object is no longer named test1.
+        with self.assertNumQueries(0):
+            Test.objects.get(name='test1')
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1a')
+
+    def test_raw_read_disabling_using_with_stmt(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        # Disable the cache
+        with DISABLE_CACHING:
+            with self.assertNumQueries(1):
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT name FROM cachalot_test where name = %s", ('test1', ))
+                    tuple(cursor.fetchall())
+        # Caching enabled, invalidating has run
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+
+    def test_raw_read_disabling_without_invalidating_using_with_stmt(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        # Disable the cache
+        with self.assertNumQueries(1):
+            with DISABLE_CACHING:
+                DISABLE_CACHING.do_not_invalidate()
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT name FROM cachalot_test where name = %s", ('test1', ))
+        # Caching enabled, invalidating has run
+        with self.assertNumQueries(0):
+            Test.objects.get(name='test1')
+
+    def test_raw_read_disabling_using_try_finally(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        blew_up = False
+        error_message = None
+        # Disable the cache
+        try:
+            with self.assertNumQueries(1):
+                DISABLE_CACHING.enable()
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT name FROM cachalot_test where name = %s", ('test1', ))
+        except Exception as err:  # In python 3 err is deleted after this block
+            blew_up = True
+            error_message = err
+        finally:
+            DISABLE_CACHING.disable()
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(error_message))
+        # Caching enabled but invalidating has run
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+
+    def test_raw_read_disabling_without_invalidating_using_try_finally(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        blew_up = False
+        error_message = None
+        # Disable the cache
+        try:
+            DISABLE_CACHING.enable()
+            with self.assertNumQueries(1):
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT name FROM cachalot_test where name = %s", ('test1', ))
+        except Exception as err:  # In python 3 err is deleted after this block
+            blew_up = True
+            error_message = err
+        finally:
+            DISABLE_CACHING.disable(invalidate_cache=False)
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(error_message))
+        # Caching enabled without invalidating so this query should be cached
+        with self.assertNumQueries(0):
+            Test.objects.get(name='test1')
+
+    def test_raw_write_disabling_using_with_stmt(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        # Disable the cache
+        with DISABLE_CACHING:
+            with self.assertNumQueries(1):
+                with connection.cursor() as cursor:
+                    cursor.execute("UPDATE cachalot_test set name = %s where name = %s", ('test1a', 'test1'))
+        # Caching enabled, invalidating has run
+        with self.assertRaises(Test.DoesNotExist):
+            Test.objects.get(name='test1')
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1a')
+
+    def test_raw_write_disabling_without_invalidating_using_with_stmt(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        # Disable the cache
+        with DISABLE_CACHING:
+            DISABLE_CACHING.do_not_invalidate()
+            with self.assertNumQueries(1):
+                with connection.cursor() as cursor:
+                    cursor.execute("UPDATE cachalot_test set name = %s where name = %s", ('test1a', 'test1'))
+        # Caching enabled without invalidating so this query should
+        # be cached even though the object is no longer named test1.
+        with self.assertNumQueries(0):
+            Test.objects.get(name='test1')
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1a')
+
+    def test_raw_write_disabling_using_try_finally(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
         blew_up = False
         err = None
         # Disable the cache
         try:
             DISABLE_CACHING.enable()
             with self.assertNumQueries(1):
-                data1.name = 'test1a'
-                data1.save()
+                with connection.cursor() as cursor:
+                    cursor.execute("UPDATE cachalot_test set name = %s where name = %s", ('test1a', 'test1'))
+        except Exception as err:
+            blew_up = True
+        finally:
+            DISABLE_CACHING.disable()
+        self.assertFalse(blew_up, msg='Unexpected Exception Occurred: {0}'.format(err))
+        # Caching enabled, invalidating has run
+        with self.assertRaises(Test.DoesNotExist):
+            Test.objects.get(name='test1')
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1a')
+
+    def test_raw_write_disabling_without_invalidating_using_try_finally(self):
+        with self.assertNumQueries(1):
+            Test.objects.get(name='test1')
+        blew_up = False
+        err = None
+        # Disable the cache
+        try:
+            DISABLE_CACHING.enable()
+            with self.assertNumQueries(1):
+                with connection.cursor() as cursor:
+                    cursor.execute("UPDATE cachalot_test set name = %s where name = %s", ('test1a', 'test1'))
         except Exception as err:
             blew_up = True
         finally:
@@ -241,41 +349,6 @@ class DisablingTestCase(TestUtilsMixin, TransactionTestCase):
         # Caching enabled without invalidating so this query should
         # be cached even though the object is no longer named test1.
         with self.assertNumQueries(0):
-            data2 = Test.objects.get(name='test1')
-            data2_name = data2.name  # Force the query to run
+            Test.objects.get(name='test1')
         with self.assertNumQueries(1):
-            data3 = Test.objects.get(name='test1a')
-            data3_name = data3.name  # Force the query to run
-        data1.name = 'test1'
-        data1.save()
-
-    def test_raw_read_toggle_disabling_using_with_stmt(self):
-        with self.assertNumQueries(1):
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
-        # Disable the cache
-        with DISABLE_CACHING:
-            with self.assertNumQueries(1):
-                with connection.cursor() as cursor:
-                    cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
-        # Caching enabled, invalidating has run
-        with self.assertNumQueries(1):
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
-        with self.assertNumQueries(0):
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
-
-    def test_raw_read_toggle_disabling_without_invalidating_using_with_stmt(self):
-        with self.assertNumQueries(1):
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
-        # Disable the cache
-        with DISABLE_CACHING:
-            with self.assertNumQueries(1):
-                with connection.cursor() as cursor:
-                    cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
-        # Caching enabled, invalidating has run
-        with self.assertNumQueries(0):
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT name FROM cachalot_test where name = %s", 'test1')
+            Test.objects.get(name='test1a')
