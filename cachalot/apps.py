c9701cf0d19ebdf9f95108d23fc291cc7bfa23bd
Federico Bond <federicobond@gmail.com>
2019-04-02 10:54:11 -0300

    Add Django 2.2 to test matrix

--- cachalot/apps.py
+++ cachalot/apps.py
@@ -13,7 +13,7 @@ from .settings import (
 
 @register(Tags.compatibility)
 def check_django_version(app_configs, **kwargs):
-    if not (1, 11) <= django_version < (2, 2):
+    if not (1, 11) <= django_version < (2, 3):
         return [Error(
             'Django %s is not compatible with this version of django-cachalot.'
             % django__version__,
