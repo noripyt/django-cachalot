b844820791971d6df4bce374247e697bf5762054
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2020-10-16 09:38:00 -0400

    Update docs for signal
    In PR #165

--- cachalot/signals.py
+++ cachalot/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
-# The argument "providing_args=['db_alias']" is deprecated and purely documentational.
-# TODO: Write a explaining text to what "db_alias" is supposed to document.
+# sender: name of table invalidated
+# db_alias: name of database that was effected
 post_invalidation = Signal()
