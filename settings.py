6108d8e5b6fbffe91e404005b42618eb5211df25
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-04-30 14:26:28 -0400

    Use GitHub actions & remove Travis (#173)

--- settings.py
+++ settings.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-
-from __future__ import unicode_literals
 import os
 
 
@@ -13,15 +10,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cachalot',
         'USER': 'cachalot',
+        'HOST': '127.0.0.1',
     },
     'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cachalot',
         'USER': 'root',
+        'HOST': '127.0.0.1',
     },
 }
 if 'MYSQL_PASSWORD' in os.environ:
     DATABASES['mysql']['PASSWORD'] = os.environ['MYSQL_PASSWORD']
+if 'POSTGRES_PASSWORD' in os.environ:
+    DATABASES['postgresql']['PASSWORD'] = os.environ['POSTGRES_PASSWORD']
 for alias in DATABASES:
     if 'TEST' not in DATABASES[alias]:
         test_db_name = 'test_' + DATABASES[alias]['NAME']
