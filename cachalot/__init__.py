6796759d0cf784af1d4e62f10158676faef8e33d
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-08-21 08:51:18 -0400

    Fix ImportError from setup.py

--- cachalot/__init__.py
+++ cachalot/__init__.py
@@ -1,7 +1,10 @@
-from django import DJANGO_VERSION
-
 VERSION = (2, 4, 2)
 __version__ = ".".join(map(str, VERSION))
 
-if DJANGO_VERSION < (3, 2):
+try:
+    from django import VERSION as DJANGO_VERSION
+
+    if DJANGO_VERSION < (3, 2):
+        default_app_config = "cachalot.apps.CachalotConfig"
+except ImportError:
     default_app_config = "cachalot.apps.CachalotConfig"
