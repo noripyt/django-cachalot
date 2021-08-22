53c99af2c4c5eb21fb29d67f528cf5c2e5adc82d
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-08-21 20:55:33 -0400

    Fix Django 3.2 default_app_config deprecation (#198)
    * Fix ImportError from setup.py

--- cachalot/__init__.py
+++ cachalot/__init__.py
@@ -1,4 +1,10 @@
 VERSION = (2, 4, 2)
 __version__ = ".".join(map(str, VERSION))
 
-default_app_config = "cachalot.apps.CachalotConfig"
+try:
+    from django import VERSION as DJANGO_VERSION
+
+    if DJANGO_VERSION < (3, 2):
+        default_app_config = "cachalot.apps.CachalotConfig"
+except ImportError:
+    default_app_config = "cachalot.apps.CachalotConfig"
