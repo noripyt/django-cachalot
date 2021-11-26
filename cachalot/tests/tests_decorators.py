5ed84535c1e323b6221ad6f3e5b12f6845f15b29
Pavel Pančocha <pp@zemtu.com>
2021-11-26 20:56:21 +0100

    Add decorators for testing different CACHALOT_FINAL_SQL_CHECK settings

---
+++ cachalot/tests/tests_decorators.py
@@ -0,0 +1,44 @@
+import logging
+from functools import wraps
+from django.test.utils import override_settings
+
+logger = logging.getLogger(__name__)
+
+
+def all_final_sql_checks(func):
+    """
+    Runs test as two sub-tests:
+    one with CACHALOT_FINAL_SQL_CHECK setting True, one with False
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        for i, sql_check in enumerate((True, False)):
+            with self.subTest(msg=f'CACHALOT_FINAL_SQL_CHECK = {sql_check}'):
+                with override_settings(CACHALOT_FINAL_SQL_CHECK=sql_check):
+                    func(self, *args, **kwargs)
+
+    return wrapper
+
+
+def no_final_sql_check(func):
+    """
+    Runs test with CACHALOT_FINAL_SQL_CHECK = False
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        with override_settings(CACHALOT_FINAL_SQL_CHECK=False):
+            func(self, *args, **kwargs)
+
+    return wrapper
+
+
+def with_final_sql_check(func):
+    """
+    Runs test with CACHALOT_FINAL_SQL_CHECK = True
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        with override_settings(CACHALOT_FINAL_SQL_CHECK=True):
+            func(self, *args, **kwargs)
+
+    return wrapper
