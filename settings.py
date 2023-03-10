2ae5cd17df5ca22fc5f552652aeeac96c8738232
Petr Dlouhý <petr.dlouhy@email.cz>
2023-03-10 22:51:56 +0100

    try to get around problem with PASSWORD in GitHub actions testing

--- settings.py
+++ settings.py
@@ -12,6 +12,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cachalot',
         'USER': 'cachalot',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
     },
     'mysql': {
